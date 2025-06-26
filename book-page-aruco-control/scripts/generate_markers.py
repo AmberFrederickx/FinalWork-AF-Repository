42e6136baa5e9f699b8320c9f8e8bb6fdbe5ec47
AmberFrederickx <71830679+AmberFrederickx@users.noreply.github.com>
2025-06-26 17:37:48 +0200

    Fixed generate markers. Now saves inside main folder.

--- book-page-aruco-control/scripts/generate_markers.py
+++ book-page-aruco-control/scripts/generate_markers.py
@@ -6,9 +6,11 @@ if not hasattr(cv2, 'aruco'):
     raise ImportError("cv2.aruco module is not available. Please install opencv-contrib-python.")
 
 from cv2 import aruco
+from pathlib import Path
 
 # Configuration
-output_dir = "../markers"
+base_dir = Path(__file__).resolve().parent.parent
+output_dir = base_dir/"markers"
 dictionary_id = aruco.DICT_6X6_250
 num_markers = 10  # Adjust to number of pages you want
 
@@ -18,7 +20,7 @@ os.makedirs(output_dir, exist_ok=True)
 
 try:
     for marker_id in range(num_markers):
-              marker_img = aruco.drawMarker(aruco_dict, marker_id, 700)
+        marker_img = aruco.drawMarker(aruco_dict, marker_id, 700)
         filepath = os.path.join(output_dir, f"marker_{marker_id}.png")
         cv2.imwrite(filepath, marker_img)
     print(f"Saved {num_markers} markers to {output_dir}")
